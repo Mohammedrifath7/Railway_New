be3f2f7676c2d94c844a7d963ebbfff351e139d2
bemebe <mohammedrifath.@email.com>
2024-11-12 23:56:19 +0530

    Pushed

--- rail-madad-clone/src/App.jsx
+++ rail-madad-clone/src/App.jsx
@@ -5,24 +5,24 @@ import SubmissionSuccess from "./components/Submission/SubmissionSuccess";
 import DepartmentCheck from './components/Department/DepartmentCheck';
 import './App.css';
 
+// Import images
+import defaultImage from './assets/pexels-pixabay-72594.jpg';
+import successImage from './assets/denis-chick-mHqIs22M2Kw-unsplash.jpg';
+import departmentImage from './assets/sugden-guy-sugden-JcimvPDC3as-unsplash.jpg';
+
 const App = () => {
   const location = useLocation();
 
   const getBackgroundImage = () => {
     switch (location.pathname) {
       case '/submission-success':
-        return "url('/src/assets/denis-chick-mHqIs22M2Kw-unsplash.jpg')";
+        return `url(${successImage})`;
       case '/department-check':
-        return "url('/src/assets/sugden-guy-sugden-JcimvPDC3as-unsplash.jpg')";
+        return `url(${departmentImage})`;
       default:
-        return "url('/src/assets/pexels-pixabay-72594.jpg')";
+        return `url(${defaultImage})`;
     }
-    
   };
-  
-  
-  
-  
 
   return (
     <div className="App" style={{ backgroundImage: getBackgroundImage() }}>
